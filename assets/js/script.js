7ec07393e90b459a792e0f422b3588a8ddc840f1
Andrew Coon <thenran@gmail.com>
2021-02-23 01:25:17 -0500

    Added function to populate timesheet, initialize timesheet from localstorage or empty. Begun work on save funciton.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,20 @@
 var currentEl = $("#currentDay");
 var timeTableEl = $(".container");
 
-// Generate and display timetable elements.
-function createTableEl() {
-    for(i = 9; i < 18; i++){
-        var rowEl = $("<div></div>").addClass("row");
-        var hourEl = $("<div></div>").addClass("hour col-1");
-        var timeSlotEl = $("<div></div>").attr({"id": i, "class": "col-8"}).append($("<textarea></textarea"));
-        var saveBtnEl = $("<button></button>").attr({"type": "button", "class": "col-1 saveBtn"}).append($("<i></i>").text("Save!"));
+// Fetch timesheet from localstorage if there is one, or initialize to empty array
+var timesheet = JSON.parse(localStorage.getItem("timesheet")) || ["","","","","","","","",""];
 
+// Generate and display timetable elements.
+function createTimesheet() {
+
+    // Loop to create multiple rows of elements
+    for(var i = 9; i < 18; i++){
+        var rowEl = $("<div></div>").addClass("row");
+        var hourEl = $("<div></div>").addClass("hour col");
+        var timeSlotEl = $("<div></div>").attr({"id": i, "class": "col-10"}).append($("<textarea></textarea"));
+        var saveBtnEl = $("<button></button>").attr({"type": "button", "class": "col saveBtn"}).append($("<i></i>").text("Save!"));
+
+        // Format text label per each time slot
         if(i < 12){
             hourEl.text(i + "AM");
         }
@@ -24,7 +30,33 @@ function createTableEl() {
     }
 }
 
+// Populate timesheet
+function populateTimesheet() {
+    
+    var textBoxes = $("textarea");
+
+    for (var i = 0; i < timesheet.length; i++) {
+        $(textBoxes[i]).text(timesheet[i]);
+    }
+}
+
+// Save a given time block
+function saveTimeBlock(event) {
+    // Determine which time block
+    // var hourBlock = $(event.target + "div");
+    // var id = parseInt(hourBlock.attr("id"), 10) - 9;
+
+    console.log(event);
+    
+    // Save contents of a timeblock to timesheet array, then save to localstorage
+    // timesheet[id] = $(hourBlock > "textarea").text();
+    // localStorage.setItem("timesheet", JSON.stringify(timesheet));
+}
+
 // Display current day
 currentEl.text(moment().format("dddd, MMMM Do"));
 
-createTableEl();
+createTimesheet();
+populateTimesheet();
+
+$(".saveBtn").on("click", saveTimeBlock(event));
